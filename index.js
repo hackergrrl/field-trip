761cf89360c9d029777f88563ace0c98b8d91910
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-06-07 11:02:28 -0700

    works

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = function (origins, visit, finish) {
+  var pending = 0
+  var finished = false
+
+  function walk (key) {
+    pending++
+
+    visit(key, add, done)
+
+    var queue = []
+
+    function add (item) {
+      queue.push(item)
+    }
+
+    function done (err) {
+      if (finished) {
+        return
+      }
+      if (err) {
+        finished = true
+        return finish(err)
+      }
+
+      if (queue.length > 0) {
+        queue.forEach(walk)
+      } else {
+        if (--pending === 0) {
+          finish()
+        }
+      }
+    }
+  }
+
+  origins.forEach(walk)
+}
